31100be9bce03a18e393e857b331d279a0ae3bb1
Marlon Jr <marlonjr787@hotmail.com>
2024-05-12 17:03:11 +0800

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,13 @@ import Home from "./pages/Home.jsx";
 import Signup from "./pages/Signup.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import About from "./pages/About.jsx";
+import Header from "./components/Header.jsx";
 
+//Create routes
 export default function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
